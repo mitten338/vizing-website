4af78d3ec43f7a382f2f933c96e7c8b33f87bf30
Mitten338 <mitten338@gmail.com>
2024-10-21 22:28:50 +0800

    feat: add background pattern image

--- src/app/boundless-travel/page.tsx
+++ src/app/boundless-travel/page.tsx
@@ -6,13 +6,18 @@ import BoundlessTravelContent from "@/app/boundless-travel/component/Travel/inde
 // atom
 import { useAtom } from "jotai";
 import { combinedTravelInfoAtom } from "@/atoms/accountAtom";
+// assets
+import TravelBgPattern from "@/assets/images/boundless-travel/travel-bg-pattern.svg";
 
 export default function BoundlessTravel() {
   const [combindedTravelInfo, setCombindedTravelInfo] = useAtom(combinedTravelInfoAtom);
 
   return (
-    <div className="w-full mt-[64px]">
-      {combindedTravelInfo.isWelcomeViewed ? <BoundlessTravelContent /> : <WelcomePeriod />}
+    <div className="relative w-full mt-[64px]">
+      <TravelBgPattern className="absolute z-1 top-[-616px] left-[-70px]" />
+      <div className="relative z-2">
+        {combindedTravelInfo.isWelcomeViewed ? <BoundlessTravelContent /> : <WelcomePeriod />}
+      </div>
     </div>
   );
 }
