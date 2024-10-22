4c9a1f7f4861b5d2a8e1a65ef99b6064c56b7ccd
Mitten338 <mitten338@gmail.com>
2024-10-22 18:53:30 +0800

    fix: add Suspense for using useSearchParams

--- src/app/boundless-travel/page.tsx
+++ src/app/boundless-travel/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import WelcomePeriod from "@/app/boundless-travel/component/WelcomePeriod/index";
 import BoundlessTravelContent from "@/app/boundless-travel/component/Travel/index";
 // atom
@@ -13,11 +13,13 @@ export default function BoundlessTravel() {
   const [combindedTravelInfo, setCombindedTravelInfo] = useAtom(combinedTravelInfoAtom);
 
   return (
-    <div className="relative w-full mt-[64px]">
-      <TravelBgPattern className="absolute z-1 top-[-616px] left-[-70px]" />
-      <div className="relative z-2">
-        {combindedTravelInfo.isWelcomeViewed ? <BoundlessTravelContent /> : <WelcomePeriod />}
+    <Suspense>
+      <div className="relative w-full mt-[64px]">
+        <TravelBgPattern className="absolute z-1 top-[-616px] left-[-70px]" />
+        <div className="relative z-2">
+          {combindedTravelInfo.isWelcomeViewed ? <BoundlessTravelContent /> : <WelcomePeriod />}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
