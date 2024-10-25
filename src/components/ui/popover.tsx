8f5d3958f626711fe061f9b9acd1a9eb04c3c957
Mitten338 <mitten338@gmail.com>
2024-10-25 11:20:28 +0800

    feat: style and assets update

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -23,7 +23,7 @@ const PopoverComponent: FC<PopoverProps> = ({
       <Popover.Trigger>{trigger}</Popover.Trigger>
       <Popover.Portal>
         <Popover.Content
-          className="flex flex-col rounded-[12px] min-w-[203px] bg-[#232021] content-between shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
+          className="flex flex-col rounded-[12px] min-w-[203px] bg-[#232021] content-between shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
           side={side}
           align={align}
           sideOffset={sideOffset}
